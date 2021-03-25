6501be925a14e6fe017b5ce5ea4092aba7e91012
Your NameAlex <alehr45@gmail.com>
2021-03-25 13:10:42 -0500

    set up homepage with handlebars

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require('express');
-const routes = require('./routes');
+const controllers = require('./controllers');
 const sequelize = require('./config/connection');
-
+const path = require('path');
 const app = express();
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// turn on routes
-app.use(routes);
+app.use(express.static(path.join(__dirname, 'public')));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.use(controllers);
 
 // turn on connection to db and server
 sequelize.sync({ force: false}).then(() => {
