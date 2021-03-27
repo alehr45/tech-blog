e76e9b816d668097cbf59fc3d81b4cac1697091e
Your NameAlex <alehr45@gmail.com>
2021-03-27 12:52:14 -0500

    added edit and delete post functionality

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection');
-const { Post, User, Comment } = require('../../models');
+const { Post, User, Comment, Vote } = require('../../models');
+const withAuth = require('../../utils/auth');
 
 // get all users
 router.get('/', (req, res) => {
@@ -11,8 +12,8 @@ router.get('/', (req, res) => {
       'post_url',
       'title',
       'created_at',
-],
-    order: [['created_at', 'DESC']],
+      
+    ],
     include: [
       {
         model: Comment,
@@ -45,7 +46,7 @@ router.get('/:id', (req, res) => {
       'post_url',
       'title',
       'created_at',
-
+      
     ],
     include: [
       {
@@ -75,7 +76,7 @@ router.get('/:id', (req, res) => {
     });
 });
 
-router.post('/', (req, res) => {
+router.post('/', withAuth, (req, res) => {
   // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
   Post.create({
     title: req.body.title,
@@ -89,9 +90,17 @@ router.post('/', (req, res) => {
     });
 });
 
+router.put('/upvote', withAuth, (req, res) => {
+  // custom static method created in models/Post.js
+  Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+    .then(updatedVoteData => res.json(updatedVoteData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
-
-router.put('/:id', (req, res) => {
+router.put('/:id', withAuth, (req, res) => {
   Post.update(
     {
       title: req.body.title
@@ -115,7 +124,8 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', withAuth, (req, res) => {
+  console.log('id', req.params.id);
   Post.destroy({
     where: {
       id: req.params.id
