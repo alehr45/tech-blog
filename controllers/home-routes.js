b462a483631bc9d8802d77c24ad3e9bd04e54f26
Your NameAlex <alehr45@gmail.com>
2021-03-27 11:39:21 -0500

    login allows users access to certain elements

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -24,7 +24,10 @@ router.get("/", (req, res) => {
     .then((dbPostData) => {
       console.log(dbPostData[0]);
       const posts = dbPostData.map(post => post.get({ plain: true }));
-      res.render('homepage', { posts });
+      res.render('homepage', {
+        posts,
+        loggedIn: req.session.loggedIn
+      });
     })
     .catch((err) => {
       console.log(err);
@@ -69,7 +72,10 @@ router.get('/post/:id', (req, res) => {
       const post = dbPostData.get({ plain: true });
 
       // pass data to template
-      res.render('single-post', { post });
+      res.render('single-post', {
+        post,
+        loggedIn: req.session.loggedIn
+      });
     })
     .catch(err => {
       console.log(err);
